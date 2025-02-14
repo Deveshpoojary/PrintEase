42bd1c173fe13bedc9fc7712faf825b704c6f49d
Deveshpoojary <deveshpoojary05@gmail.com>
2025-02-14 18:43:41 +0530

    Added admin panel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import Dashboard from "./pages/Dashboard";
 import NotFoundPage from './components/NotFounPage';
+import AdminDashboard from './admin/AdminDashboard';
+import LoginPage from './pages/Login';
+import SignUpPage from './pages/Signup';
+
 
 function App() {
   return (
